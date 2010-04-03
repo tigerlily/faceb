f039ff62f1ad894030a20f1aeacf86555876da19
Mathieu Fosse <mathieu.fosse@gmail.com>
2010-04-03 11:29:11 +0200

    Typo

--- lib/faceb/rails.rb
+++ lib/faceb/rails.rb
@@ -4,7 +4,7 @@ if Rails.version >= "3.0"
 elsif Rails.version >= "2.3"
   require 'faceb/rails/rails_23_init'
 else
-  raise "Sorry, but FaceB need Rails 2.3 or later"
+  raise "Sorry, but FaceB needs Rails 2.3 or later"
 end
 
 require 'faceb/rails/base'
